ed55763a0401621633ff5926df09004c657d9b8b
bmarieay <marieantonettebacay@gmail.com>
2022-03-15 20:47:30 -0700

    added resume modal

--- modals.js
+++ modals.js
@@ -1,11 +1,12 @@
 const modalTogglers = document.querySelectorAll('.project__link');
 const closeButtons = document.querySelectorAll('.modal-close');
+const resumeToggler = document.querySelector('.resume');
+
 
 modalTogglers.forEach( modalToggler => {
     modalToggler.addEventListener('click', e => {
         e.preventDefault();
         const modal = modalToggler.parentNode.parentNode.nextElementSibling;
-        // console.log(modal)
         modal.classList.add('is-open');
         const closeBtn = modal.querySelector('.modal-close');
         closeBtn.addEventListener('click', () => {
@@ -14,21 +15,12 @@ modalTogglers.forEach( modalToggler => {
     })
 })
 
-
-// for(let toggler of modalTogglers){
-//     toggler.addEventListener('click', () => {
-//         const modal = modalToggle.parentNode.nextElementSibling;
-
-//     })
-// }
-
-// projectContainter.addEventListener('click', e => {
-//     e.preventDefault();
-//     console.log(e.target)
-//     const modalToggle = e.target.closest('.project__link');
-//     console.log(modalToggle)
-//     if(!modalToggle) 
-//         return
-    
-//     const modal = modalToggle.parentNode.nextElementSibling;
-// })
+resumeToggler.addEventListener('click', e => {
+    e.preventDefault();
+    const modal = document.querySelector('.modal-resume');
+    modal.classList.add('is-open');
+    const closeBtn = modal.querySelector('.modal-close');
+    closeBtn.addEventListener('click', () => {
+        modal.classList.remove('is-open');
+    })
+})
