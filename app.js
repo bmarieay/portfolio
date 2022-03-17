32298ca000724c07812fb18d0dfdd68ddf9e8687
bmarieay <marieantonettebacay@gmail.com>
2022-03-16 22:45:16 -0700

    added grid to projects and modified themes

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const t1 = new TimelineMax();
 
 t1.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 .fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut});
-// .fromTo(primaryNav, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5");
 
 //navbar toggling
 navToggle.addEventListener('click', () => {
